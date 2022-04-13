abdc2f2129ee5ea95f424494946e38b252532c5d
Andrew Park <AndrewJHPark@protonmail.com>
2022-04-13 17:19:24 +0900

    04/13/22

--- 7kyu.js
+++ 7kyu.js
@@ -60,3 +60,45 @@ function disemvowel(str) {
     
     return output;
 }
+
+// Sort Numbers
+
+//Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+function solution(nums){
+  if(nums){
+    return nums.sort((a,b)=> a-b)
+  }else{
+    return []
+  }
+}
+
+
+// 'x' marks the spot.
+// #'x' marks the spot
+// ##Task:
+// Given a two dimensional array, return the co-ordinates of 'x'.
+// If 'x' is not inside the array, or if 'x' appears multiple times, return []
+// The co-ordinates should be zero indexed.
+// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+const xMarksTheSpot = (input) => {
+  let cords=[]
+  for (let i=0; i<input.length; i++){
+    console.log(i, ' i')
+    if (input[i].includes('x')){
+      for (let j=0; j<input[i].length; j++){
+        console.log(j, ' j')
+        console.log(cords, ' cords')
+        if(input[i][j]=='x'){
+          cords = cords.concat([i,j])
+        }
+      }
+    }
+  }
+  if (cords.length == 0 || cords.length > 2){
+    return [];
+  } else {
+    return cords
+  }
+}
