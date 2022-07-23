9d6145f341376957ff38687cd3460c2631cf843b
parkshub <AndrewJHPark@protonmail.com>
2022-07-23 17:50:27 +0900

    07.23.22

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -357,3 +357,15 @@ var preorderTraversal = function(root) {
   }
   return res
 }
+
+// #18
+// postorder resursive
+var postorderTraversal = function(root, arr = new Array()) {
+      if (root === null) return [];
+      
+      postorderTraversal(root.left, arr)
+      postorderTraversal(root.right, arr)
+      arr.push(root.val)
+      
+      return arr
+  };
