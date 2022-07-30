5bd31a6cd60a9c325156ea8150d21f16c03e0c37
parkshub <AndrewJHPark@protonmail.com>
2022-07-29 22:55:34 -0700

    07.29.22

--- codewars/4kyu-recoverSecret.js
+++ codewars/4kyu-recoverSecret.js
@@ -1,4 +1,6 @@
-var recoverSecret = function(triplets) {
+// iterative
+
+var recoverSecretV1 = function(triplets) {
   
     let arr = []
   
@@ -15,3 +17,29 @@ var recoverSecret = function(triplets) {
     }
     return arr.join('')
   }
+
+
+// recursive
+
+let triplets1 = [
+    ['t','u','p'],
+    ['w','h','i'],
+    ['t','s','u'],
+    ['a','t','s'],
+    ['h','a','p'],
+    ['t','i','s'],
+    ['w','h','s']
+  ]
+
+
+var recoverSecretV2 = function(triplets) {
+    for ([first] of triplets) {
+        if (triplets.every(x => x.indexOf(first) <= 0)) {
+            triplets.filter(([item]) => item == first).forEach(tuple => tuple.shift())
+            return first + recoverSecretV2(triplets.filter(tuples => tuples.length > 0))
+        }
+    }
+    return ''
+}
+
+console.log(recoverSecretV2(triplets1))
