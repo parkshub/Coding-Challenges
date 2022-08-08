6860d45aef2e31c3245e7b1e7becee036a4650b2
parkshub <AndrewJHPark@protonmail.com>
2022-08-08 15:17:47 +0900

    08.08.22

--- codewars/test.js
+++ codewars/test.js
@@ -1,13 +1,1 @@
-let x = 'aa b'
-
-let arr = new Array(122-96)
-
-for (let i = 0; i < arr.length; i++) {arr[i] = String.fromCharCode(97+i)}
-
-let answer = x.split(' ').map(word => word.split('').reduce((prev, curr) => {
-    return prev + arr.indexOf(curr) + 1
-},0))
-
-let idx = answer.indexOf(Math.max(...answer))
-
-console.log(x.split(' ')[idx])
+'testing'
