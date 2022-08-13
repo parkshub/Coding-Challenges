5c0eac07dc876e2693a8bb59974b0d3c941827dd
parkshub <AndrewJHPark@protonmail.com>
2022-08-13 17:27:13 +0900

    08.13.22

--- codewars/test.js
+++ codewars/test.js
@@ -1,9 +1,2 @@
-let array = [ 5, 3, 2, 8, 1, 4 ]
+let a = 'string'
 
-let sortedArr = array.filter(x => x%2 != 0).sort((a,b) => b-a)
-
-for (let i = 0; i<array.length; i++) {
-    if (array[i]%2!=0) array[i] = sortedArr.pop();
-}
-
-console.log(array)
