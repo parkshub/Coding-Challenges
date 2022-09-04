9671607a2ca9694e807c650977d6d26e35668ed2
parkshub <AndrewJHPark@protonmail.com>
2022-09-04 14:34:55 +0900

    09.04.22

--- leetcode/test.js
+++ leetcode/test.js
@@ -1,21 +1,5 @@
 p = console.log
 
-let str = 'abcde'
-// let str = 'abcde'
+let a =[[ 1, 2 ], [null]]
 
-let res = []
-
-str.split('')
-
-for(let i=0; i<str.length; i+=2) {
-    if (i === str.length-1 && str.length%2 != 0) {
-        res.push(str[i]+'_')    
-    }
-    else {
-    res.push(str[i]+str[i+1])
-    }
-}
-
-// res[res.length-1] = res[res.length-1].replace('undefined','_')
-
-p(res)
+p(a.includes(null))
