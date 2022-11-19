22a6a0e818be4e3564a98e2e5fbf10eb2246a0bc
parkshub <AndrewJHPark@protonmail.com>
2022-11-19 17:01:28 +0900

    11.19.22

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -775,7 +775,6 @@ var isPalindrome = function(s) {
   return s.reverse().join('') == copy.join('')
 };
 
-
 var searchInsert = function(nums, target) {
   if (target < nums[0]) { return 0 }
   if (target > nums[nums.length - 1]) { return nums.length }
