d51362c99928d8fbf9c5109bdb23d6450d895dad
parkshub <AndrewJHPark@protonmail.com>
2023-03-16 17:26:05 +0900

    03.16.23

--- codewars/practice.js
+++ codewars/practice.js
@@ -140,3 +140,20 @@ function solve(s){
   
   return s.toLowerCase()
 }
+
+// Calculating with functions
+function zero(func) { return func ? func(0) : 0}
+function one(func) { return func ? func(1) : 1}
+function two(func) { return func ? func(2) : 2}
+function three(func) { return func ? func(3) : 3}
+function four(func) { return func ? func(4) : 4}
+function five(func) { return func ? func(5) : 5}
+function six(func) { return func ? func(6) : 6}
+function seven(func) { return func ? func(7) : 7}
+function eight(func) { return func ? func(8) : 8}
+function nine(func) { return func ? func(9) : 9}
+
+function plus(x) { return function(y) { return y + x }}
+function minus(x) { return function(y) { return y - x }}
+function times(x) { return function(y) { return y * x }}
+function dividedBy(x) { return function(y) { return Math.floor(y / x) }}
--- codewars/test.js
+++ codewars/test.js
@@ -1,7 +1,20 @@
-let s = 'COD'
+function zero(fn) {return fn ? fn(0) : 0}
+function one(fn) {return fn ? fn(1) : 1}
+function two(fn) {return fn ? fn(2) : 2}
+function three(fn) {return fn ? fn(3) : 3}
+function four(fn) {return fn ? fn(4) : 4}
+function five(fn) {return fn ? fn(5) : 5}
+function six(fn) {return fn ? fn(6) : 6}
+function seven(fn) {return fn ? fn(7) : 7}
+function eight(fn) {return fn ? fn(8) : 8}
+function nine(fn) {return fn ? fn(9) : 9}
 
-let upperReg = /[A-Z]/g
-let lowerReg = /[a-z]/g
+function plus(n) {return function(v) {return v + n}}
+function minus(n) {return function(v) {return v - n}}
+function times(n) {return function(v) {return v * n}}
+function dividedBy(n) {return function(v) {return v / n}}
 
-console.log(s.match(lowerReg))
+let a = 1.1
+
+console.log(Math.round(a))
 
