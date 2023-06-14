283463cd31918675101dd50d9118368a210074d1
parkshub <AndrewJHPark@protonmail.com>
2023-06-13 17:13:47 -0700

    linked list review

--- leetcode/test.js
+++ leetcode/test.js
@@ -330,3 +330,21 @@ const exploreMinIsland = (grid, r, c, visited) => {
 }
 
 console.log(minIsland(grid))
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c') 
+const d = new Node('d')
+
+a.next = b
+b.next = c
+c.next = d
+
+console.log(JSON.stringify(a))
