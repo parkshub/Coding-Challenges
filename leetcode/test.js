412f7c89b1881ab475461d5eb7e109d6b2628fe4
parkshub <AndrewJHPark@protonmail.com>
2023-06-27 14:34:05 -0700

    linked list test

--- leetcode/test.js
+++ leetcode/test.js
@@ -1,208 +1,3 @@
-// has path undirected graph
-
-const edges = [
-    ['i', 'j'],
-    ['k', 'i'],
-    ['m', 'k'],
-    ['k', 'l'],
-    ['o', 'n']
-]
-
-function buildGraph(edges) {
-    const graph = {}
-
-    for (let edge of edges) {
-        const [a, b] = edge
-        graph[a] ? graph[a].push(b) : graph[a] = [b]
-        graph[b] ? graph[b].push(a) : graph[b] = [a]
-    }
-
-    return graph
-}
-
-function hasPathUndirected(edges, src, dst) {
-    const visited = new Set()
-    const graph = buildGraph(edges)
-    if (graph[src] === undefined || graph[dst] === undefined) { return false }
-
-    function helper(graph, node, dst, visited) {
-        if (node === dst) { return true }
-        if (visited.has(node)) { return false }
-
-        visited.add(node)
-
-        for (let neighbor of graph[node]) {
-            if (helper(graph, neighbor, dst, visited) === true) {
-                return true
-            }
-        }
-
-        return false
-    }
-
-    return helper(graph, src, dst, visited)
-}
-
-
-const graph3 = {
-  0: [8, 1, 5],
-  1: [0],
-  5: [0, 8],
-  8: [0, 5],
-  2: [4, 3],
-  3: [2],
-  4: [2]
-}
-
-const connectedComponentsCount = (graph) => {
-    const visited = new Set()
-    let count = 0
-
-    function helper(graph, visited, node) {
-        if (visited.has(String(node))) { return false }
-        
-        visited.add(String(node))
-
-        for (let neighbor of graph[node]) {
-            helper(graph, visited, neighbor)
-        }
-
-        return true
-    }
-
-    for (let node in graph) {
-        if (helper(graph, visited, node) === true) {
-            count +=1
-        }
-    }
-    
-    return count
-}
-
-function largestComponent(graph) {
-    const visited = new Set()
-    let largest = -Infinity
-
-    function helper(graph, visited, node) {
-        if (visited.has(String(node))) { return 0 }
-        
-        visited.add(String(node))
-        
-        let size = 1
-        
-        for (let neighbor of graph[node]) {
-            size += helper(graph, visited, neighbor)
-        }
-
-        return size
-    }
-
-    for (let node in graph) {
-        let size = helper(graph, visited, node)
-        if (size > largest) { largest = size }
-    }
-
-    return largest
-}
-
-function shortestPath(graph, src, dst) {
-    const visited = new Set([ src ])
-    const queue  = [ [ src, 0 ] ]
-
-    while (queue.length) {
-        const [ current, distance ] = queue.shift()
-        if (current === dst) { return distance }
-
-        for (let neighbor of graph[current]) {
-            if (!visited.has(String(neighbor))) {
-                queue.push([ neighbor, distance + 1 ])
-                visited.add(String(neighbor))
-            }
-        }
-    }
-
-    return -1
-}
-
-const grid = [
-  ['W', 'L', 'W', 'W', 'W'],
-  ['W', 'L', 'W', 'W', 'W'],
-  ['W', 'W', 'W', 'L', 'W'],
-  ['W', 'W', 'L', 'L', 'W'],
-  ['L', 'W', 'W', 'L', 'L'],
-  ['L', 'L', 'W', 'W', 'W'],
-]
-
-// island count and minimum island
-
-function islandCount(grid) {
-    const visited = new Set()
-    let count = 0
-
-    function helper(grid, visited, r, c) {
-        if (r < 0 || r >= grid.length) { return false }
-        if (c < 0 || c >= grid[0].length) { return false }
-        if (grid[r][c] === 'W') { return false }
-        
-        const pos = r + ',' + c
-
-        if (visited.has(pos)) { return false }
-
-        visited.add(pos)
-
-        helper(grid, visited, r + 1, c)
-        helper(grid, visited, r - 1, c)
-        helper(grid, visited, r, c + 1)
-        helper(grid, visited, r, c - 1)
-
-        return true
-    }
-
-    for (let r = 0; r <= grid.length; r++) {
-        for (let c = 0; c <= grid[0].length; c++) {
-            if (helper(grid, visited, r, c) === true) {
-                count +=1
-            }
-        }
-    }
-
-    return count
-}
-
-const minIsland = (grid) => {
-    const visited = new Set()
-    let smallest = Infinity
-
-    function helper(grid, visited, r, c) {
-        if (r < 0 || r >= grid.length) { return 0 }
-        if (c < 0 || c >= grid[0].length) { return 0 }
-        if (grid[r][c] === 'W') { return 0 }
-
-        const pos = r + ',' + c
-
-        if (visited.has(pos)) return (0)
-        
-        visited.add(pos)
-
-        let size = 1
-
-        size += helper(grid, visited, r + 1, c)
-        size += helper(grid, visited, r - 1, c)
-        size += helper(grid, visited, r, c + 1)
-        size += helper(grid, visited, r, c - 1)
-
-        return size
-    }
-
-    for (let r = 0; r <= grid.length; r++) {
-        for (let c = 0; c <= grid[0].length; c++) {
-            let size = helper(grid, visited, r, c)
-            if (size < smallest && size !== 0) { smallest = size }
-        }
-    }
-
-    return smallest
-}
 class Node {
     constructor(val) {
         this.val = val
@@ -219,18 +14,90 @@ a.next = b
 b.next = c
 c.next = d
 
+const a2 = new Node(2)
+const b2 = new Node(8)
+const c2 = new Node(3)
+const d2 = new Node(7)
+
+a2.next = b2
+b2.next = c2
+c2.next = d2
+
+const sumListRec = (head) => {
+    if (head === null) { return 0 }
+    return head.val + sumListRec(head.next)
+}
+
+const sumList = (head) => {
+    let count = 0
+    let current = head
+
+    while(current !== null) {
+        count += current.val
+        current = current.next
+    }
+
+    return count
+}
+
+// return true if target exists and false otherwise
+const findTarget = (head, target) => {
+    if (head === null) { return false }
+    if (head.val === target) { return true }
+
+    return findTarget(head.next, target)
+}
+
+
+// return the index value of the target
+// if it doesn't exist, return -1
+const findByIndex = (head, index) => {
+    if (head === null) { return -1 }
+    if (index === 0) { return head.val }
+    
+    return findByIndex(head.next, index - 1)
+}
+
 const reverseList = (head) => {
     let current = head
     let prev = null
 
     while (current !== null) {
-        let next = current.next
+        const next = current.next
         current.next = prev
         prev = current
         current = next
     }
-    
+
     return prev
 }
 
-console.log(JSON.stringify(reverseList(a)))
+const zipperList = (head1, head2) => {
+    let tail = head1
+    let count = 0
+    let current1 = head1.next
+    let current2 = head2
+
+    while (current1 !== null && current2 !== null) {
+        if (count % 2 === 0) {
+            tail.next = current2
+            current2 = current2.next
+        } else {
+            tail.next = current1
+            current1 = current1.next
+        }
+        tail = tail.next
+        count +=1
+    }
+    
+    tail.next = current1 || current2
+
+    return head1
+
+    // if (current1 !== null) tail.next = current1
+    // if (current2 !== null) tail.next = current2
+
+    // return head1
+}
+
+console.log(zipperList(a, a2))
