c4c226b8e8ebeebcd215077d64a676ccad44b526
parkshub <AndrewJHPark@protonmail.com>
2023-06-29 14:47:10 -0700

    linked list practice

--- codewars/test.js
+++ codewars/test.js
@@ -1,4 +1,32 @@
-let visited = new Set()
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
 
-visited.add('asdf')
-console.log(visited.has('asdf'))
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+
+a.next = b
+b.next = c
+c.next = d
+
+const a2 = new Node(2)
+const b2 = new Node(8)
+const c2 = new Node(3)
+const d2 = new Node(7)
+
+a2.next = b2
+b2.next = c2
+c2.next = d2
+
+let tail = a
+
+let current1 = a.next
+
+tail.next.next = 'something'
+
+console.log(current1)
--- leetcode/test.js
+++ leetcode/test.js
@@ -272,4 +272,96 @@ function minIsland(grid) {
     return smallest
 }
 
-console.log(minIsland(grid))
+// console.log(minIsland(grid))
+
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+
+a.next = b
+b.next = c
+c.next = d
+
+const a2 = new Node(2)
+const b2 = new Node(8)
+const c2 = new Node(3)
+const d2 = new Node(7)
+
+a2.next = b2
+b2.next = c2
+c2.next = d2
+
+function sumList(head) {
+    if (head === null) { return 0 }
+    return head.val + sumList(head.next)
+}
+
+// target exists?
+function targetExists(head, target) {
+    if (head === null) { return false }
+    if (head.val === target) { return true }
+    return targetExists(head.next, target)
+}
+
+// find target at index
+function findIndex(head, index) {
+    if (head === null) { return -1 }
+    if (index === 0) { return head.val }
+
+    return findIndex(head.next, index - 1)
+}
+
+function reverseList(head) {
+    let prev = null
+    let current = head
+
+    while(current !== null) {
+        let next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+
+    return prev
+}
+
+function reverseListRec(head, prev = null) {
+    if (head === null) { return prev }
+    const next = head.next
+    head.next = prev
+    return reverseListRec(next, head)
+}
+
+// console.log(reverseListRec(a))
+//  null   a -- b -- c -- d
+//  prev  cur--nxt
+
+const zipperList = (head1, head2) => {
+    let tail = head1
+    let current1 = head1.next
+    let current2 = head2
+    let count = 0
+
+    while (current1 !== null && current2 !== null) {
+        if (count % 2 === 0) {
+            tail.next = current2
+            current2 = current2.next
+        } else {
+            tail.next = current1
+            current1 = current1.next
+        }
+        tail = tail.next
+        count += 1
+    }
+    return head1
+}
+
+console.log(zipperList(a, a2))
