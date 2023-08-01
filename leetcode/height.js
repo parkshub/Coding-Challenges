3fea6cd6cb5d374a017d1a8823454efdc7bfba52
parkshub <AndrewJHPark@protonmail.com>
2023-08-01 15:38:40 -0700

    height-based binary tree

---
+++ leetcode/height.js
@@ -0,0 +1,24 @@
+const input = [-10,-3,0,5,9]
+
+// left child = parent * 2 + 1
+// right child = parent * 2 + 2
+// parent = Math.floor(child - 1 / 2)
+function TreeNode(val, left, right) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+    }
+
+function bts(input) {
+    if (!input.length) { return null }
+    
+    const mid = Math.floor(input.length / 2)
+    const node = new TreeNode(input[mid])
+
+    node.left = bts(input.slice(0, mid))
+    node.right = bts(input.slice(mid + 1))
+
+    return node
+}
+
+console.log(bts(input))
