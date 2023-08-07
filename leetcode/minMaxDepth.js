44055eab80b93bb0b4fd228abf21643746e10a19
parkshub <AndrewJHPark@protonmail.com>
2023-08-07 16:00:59 -0700

    max depth

--- leetcode/minMaxDepth.js
+++ leetcode/minMaxDepth.js
@@ -6,10 +6,51 @@
 //     return Math.min( minDepth(root.left), minDepth(root.right) ) + 1
 // };
 
-// var maxDepth = function(root) {
-//     if (root === null) { return 0 }
-//     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
-// }; 
+var maxDepth = function(root) {
+    // if (root === null) { return 0 }
+    // return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+
+    // breadth first iterative solution
+
+    // if (root === null) { return 0 }
+
+    // const queue = [ root ]
+    // let level = 0
+
+    // while (queue.length !== 0) {
+    //     const len = queue.length
+    //     for (let i = 0; i < len; i++) {
+    //         const current = queue.shift()
+
+    //         if (current.left !== null) { queue.push(current.left) }
+    //         if (current.right !== null) { queue.push(current.right) }
+    //     }
+
+    //     level++
+    // }
+
+    // return level
+
+    // depth first iterative solution
+
+    // if (root === null) { return 0 }
+
+    // const stack = [ [ root, 0 ] ]
+
+    // let res = -Infinity
+
+    // while (stack.length !== 0) {
+    //     const [ node, depth ] = stack.pop()
+
+    //     res = Math.max(res, depth + 1)
+
+    //     if (node.left) { stack.push([ node.left, depth + 1 ]) }
+    //     if (node.right) { stack.push([ node.right, depth + 1 ]) }
+
+    // }
+
+    // return res
+};
 
 
 function TreeNode(val, left, right) {
